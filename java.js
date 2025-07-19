da473757040130ee3c7904ff1b804bd11edc80f5
hitesh0806 <hiteshtg812@gmail.com>
2025-07-19 21:45:53 +0530

    Upgraded code, added educational details

--- java.js
+++ java.js
@@ -1,40 +1,52 @@
 
 let i;
+let edu_qul_1, exp;
 let locstor;
 let locstor_getitem=JSON.parse(localStorage.getItem('profiles'));
 let boards=document.querySelector('#boards');
-let job_experience=document.querySelector('#job-experience');
+let job_experience=document.querySelector('#job-experience-1');
 let content=document.querySelector('#content')
 let content_2=document.querySelector('#content_2')
-function e_10()
+
+
+let edu_details={
+ board_1_value:'',
+ board_2_value:'',
+ board_3_value:'',
+ board_4_value:'',
+ place_of_study:'',
+}
+
+function e_qul(e)
 {
+  if(e==='_10_'){
   boards.innerHTML=`
   <label>10th board:</label><br>
   <input id="board_10"><br><br>
   `;
-}
-function e_10_2()
-{
+  edu_qul_1='10';
+  }
+  else if(e==='_10_2_'){
   boards.innerHTML=`
   <label>10th board:</label><br>
   <input id="board_10"><br><br>
   <label>12 board:</label><br>
   <input id="board_12"><br><br>
   `;
-}
-function e_ug()
-{
+  edu_qul_1='10+2';
+  }
+  else if(e==='ug'){
   boards.innerHTML=`
   <label>10th board:</label><br>
   <input id="board_10"><br><br>
   <label>12th board:</label><br>
   <input id="board_12"><br><br>
   <label>University/Institution (UG):</label><br>
-  <input id="board_uni"><br><br>
+  <input id="board_uni_ug"><br><br>
   `;
-}
-function e_pg()
-{
+  edu_qul_1='PG';
+  }
+  else if(e==='pg'){
   boards.innerHTML=`
   <label>10th board:</label><br>
   <input id="board_10"><br><br>
@@ -45,14 +57,59 @@ function e_pg()
   <label>University/Institution (PG):</label><br>
   <input id="board_uni_pg"><br><br>
   `;
+  edu_qul_1='PG';
+  }
+  document.getElementById('eduqul').textContent=edu_qul_1;
+  console.log(edu_details)
 }
-function experienced()
+function create_1()
 {
+let board_1=document.getElementById('board_10');
+let board_2=document.getElementById('board_12');
+let board_3=document.getElementById('board_uni_ug');
+let board_4=document.getElementById('board_uni_pg');
+let p_o_s=document.getElementById('placeofstudy');
+  edu_details.board_1_value = board_1 ? board_1.value : '';
+  edu_details.board_2_value = board_2 ? board_2.value : '';
+  edu_details.board_3_value = board_3 ? board_3.value : '';
+  edu_details.board_4_value = board_4 ? board_4.value : '';  
+  edu_details.place_of_study = p_o_s ? p_o_s.value : '';
+console.log(edu_details);
+}
+function experience(e)
+{  
+  if(e==='intern'){
     job_experience.innerHTML=`
-    <button id="less_2" onclick="less_2()" value="less than 2">Less than 2</button>
-    <button id="exp_2" onclick="2()" value="2+">2+</button>
-    <button id="exp_5" onclick="5()" value="5+">5+</button><br><br>
+    <p id="job-experience-2"></p>`;
+    exp='Intern';
+  }
+  else if(e==='fresher'){
+    job_experience.innerHTML=`
+    <p id="job-experience-2"></p>`;
+    exp='Fresher';
+  }
+  else if(e==='experienced')
+    job_experience.innerHTML=`
+    <button id="years" onclick="years('_2__')">Less than 2</button>
+    <button id="years" onclick="years('_2_')">2+</button>
+    <button id="years" onclick="years('_5_')">5+</button>
+    <p id="job-experience-2"></p>
     `;
+  document.getElementById('job-experience-2').textContent=exp;
+  console.log(edu_details)
+}
+function years(y){
+  if(y==='_2__'){
+      exp='Less than 2 years';
+  }
+  else if(y==='_2_'){
+      exp='2+';
+  }
+  else{
+      exp='5+';
+  }
+  document.getElementById('job-experience-2').textContent=exp;
+  console.log(edu_details)
 }
 function create()
 {
@@ -129,7 +186,7 @@ function login()
   {
   if(x_value===locstor_getitem[i].name_ && y_value===locstor_getitem[i].password)
   {
-    window.location.href='index-2.html';
+    window.location.href='index-1-2.html';
     break;
   }
   }
@@ -138,23 +195,21 @@ function login()
 
 function createprofile()
 {
-    window.location.href='index-1.html';
+  window.location.href='index-1.html';
 }
 function next()
 {
-    window.location.href='index-1-2.html';
+  window.location.href='index-2.html';
 }
 function backtologin()
 {
-    window.location.href='index.html';
+  window.location.href='index.html';
 }
 function back_1()
 {
-    window.location.href='index-1.html';
+  window.location.href='index-1.html';
 }
 function back_2()
 {
-    window.location.href='';
+  window.location.href='';
 }
-
-    
